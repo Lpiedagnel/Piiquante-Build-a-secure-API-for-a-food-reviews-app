6381a85d5d2815e039a736cf0ba0412ea8682ec6
Lpiedagnel <piedagnel.dev@gmail.com>
2022-07-19 11:25:12 +0200

    Add controllers to user register

--- controllers/user.js
+++ controllers/user.js
@@ -2,3 +2,55 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/User')
+
+exports.signup = async (req, res) => {
+    // our register logic goes here...
+    try {
+        // Get user Input
+        const { email, password } = req.body
+
+        // Validate user input
+        if (!(email && password)) {
+            res.status(400).send("All input is required")
+        }
+
+        // Check if user already exist
+        // Validate if user exist in our database
+        const oldUser = await User.findOne({ email })
+
+        if (oldUser) {
+            return res.status(409).send("User Already Exist. Please Login")
+        }
+
+        // Encrypt user password
+        encryptedPassword = await bcrypt.hash(password, 10)
+
+        // Create user in our database
+        const user = await User.create({
+            email: email.toLowerCase(),
+            password: encryptedPassword,
+        })
+
+        // Create token
+        const token = jwt.sign(
+            { user_id: user._id, email},
+            process.env.TOKEN_KEY,
+            {
+                expiresIn: "24h",
+            }
+        )
+        // Save user token
+        user.token = token
+
+        // Return new user
+        res.status(201).json(user)
+
+        // Send error if try fail
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+exports.login = (req, res) => {
+    // our login logic goes here
+}
--- routes/user.js
+++ routes/user.js
@@ -3,4 +3,8 @@ const router = express.Router()
 
 const userCtrl = require('../controllers/user');
 
+// Ajouter ces fonctions quand les controllers seront finis
+// router.post('/auth/signup', userCtrl.signup);
+// router.post('/auth/login', userCtrl.login);
+
 module.exports = router;
