5843d963a32fc3afc698fc3a0110be62884f6e33
Lpiedagnel <piedagnel.dev@gmail.com>
2022-08-03 11:55:05 +0200

    Install Helmet and fix the img issues which come with it

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 const express = require('express')
+const helmet = require('helmet')
 const mongoose = require('mongoose')
 const path = require('path')
 const mongoString = process.env.DATABASE_URL
@@ -22,11 +23,13 @@ database.once('connected', () => {
 const app = express()
 
 app.use(express.json())
+app.use(helmet())
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*')
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+    res.setHeader('Cross-Origin-Resource-Policy', 'same-site');
     next()
 })
 
