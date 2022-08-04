bfdc043ecd2791b3059fb781d63c0773d6d025bc
Lpiedagnel <piedagnel.dev@gmail.com>
2022-08-04 16:34:02 +0200

    Add symbol interdiction to password validator to prevent injection

--- middlewares/password-validator.js
+++ middlewares/password-validator.js
@@ -10,12 +10,13 @@ passwordSchema
     .has().lowercase()                              // Must have lowercase letters
     .has().digits(1)                                // Must have at least 1 digit
     .has().not().spaces()                           // Should not have spaces
+    .has().not().symbols()                          // Should not have symbol
 
 // Export verification
 module.exports = (req, res, next) => {
     if(passwordSchema.validate(req.body.password)) {
         next()
     } else {
-        return res.status(400).json({error : `Le mot de passe doit contenir entre 5 et 35 caractères, une lettre majuscule, une lettre majuscule et un chiffre, et ne doit pas contenir d\'espace. Il vous manque ${passwordSchema.validate(req.body.password, { list: true })}`})
+        return res.status(400).json({error : `Le mot de passe doit contenir entre 5 et 35 caractères, une lettre majuscule, une lettre majuscule et un chiffre, et ne doit pas contenir d\'espace, ni de symbole. Le problème vient de ${passwordSchema.validate(req.body.password, { list: true })}`})
     }
 }
